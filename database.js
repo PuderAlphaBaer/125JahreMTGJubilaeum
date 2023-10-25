6d2b716e96297be41e4b48184151cbd5fdb620cf
PuderAlphaBaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-10-25 21:26:36 +0200

    npm

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
-const { createClient } = supabase
-supabase = createClient('https://cqlueytrxqlhdvhqqyse.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbHVleXRyeHFsaGR2aHFxeXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNTQ5NTAsImV4cCI6MjAxMzgzMDk1MH0.Gr19SNLr2pNQFwxkHvKhK09DN-DjglQFPzbNY_p9A9o')
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://cqlueytrxqlhdvhqqyse.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbHVleXRyeHFsaGR2aHFxeXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNTQ5NTAsImV4cCI6MjAxMzgzMDk1MH0.Gr19SNLr2pNQFwxkHvKhK09DN-DjglQFPzbNY_p9A9o')
 
 console.log('Supabase Instance: ', supabase)
 // ...
