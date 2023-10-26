33e5d67f47322f4ef2736d32d2fb5093b51ae0c9
Frank Walter <123561496+INFOMEBIS@users.noreply.github.com>
2023-10-26 20:04:28 +0200

    js bt angefangen

--- indexScript.js
+++ indexScript.js
@@ -1,10 +1,17 @@
 const bt1 = document.getElementById('bt1');
 const tb1 = document.getElementById('tb1');
+const bta = document.getElementById('bta')
+const btb = document.getElementById('btb')
+const btc = document.getElementById('btc')
+const btd = document.getElementById('btd')
 var nickname;
 
 
 bt1.addEventListener('click', nicknameuebermitteln());
-
+bta.addEventListener('click', )
+btb.addEventListener('click', )
+btc.addEventListener('click', )
+btd.addEventListener('click', )
 
 function nicknameuebermitteln() {
     nickname = tb1.value;
