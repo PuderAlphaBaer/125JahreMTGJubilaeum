720002d56ad3db0a5251d5e6d383542809e6fb9d
Frank Walter <123561496+INFOMEBIS@users.noreply.github.com>
2023-10-26 18:00:44 +0200

    submitbutton neu

--- indexScript.js
+++ indexScript.js
@@ -1,1 +1,11 @@
- 
+const bt1 = document.getElementById('bt1');
+const tb1 = document.getElementById('tb1');
+var nickname;
+
+
+bt1.addEventListener('click', nicknameuebermitteln());
+
+
+function nicknameuebermitteln() {
+    nickname = tb1.value;
+}
