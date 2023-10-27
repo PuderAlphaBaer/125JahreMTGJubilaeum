943ecbc9dee34ececda11784eeda3743cb08b348
chriveloper <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-10-27 09:51:49 +0200

    tr

--- database.js
+++ database.js
@@ -1,4 +1,7 @@
+const { createClient } = supabase;
 
-const supaURL = 'https://cqlueytrxqlhdvhqqyse.supabase.co'
-const supaKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbHVleXRyeHFsaGR2aHFxeXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNTQ5NTAsImV4cCI6MjAxMzgzMDk1MH0.Gr19SNLr2pNQFwxkHvKhK09DN-DjglQFPzbNY_p9A9o'
+const supaUrl = 'https://cqlueytrxqlhdvhqqyse.supabase.co'
+const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbHVleXRyeHFsaGR2aHFxeXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNTQ5NTAsImV4cCI6MjAxMzgzMDk1MH0.Gr19SNLr2pNQFwxkHvKhK09DN-DjglQFPzbNY_p9A9o'
 
+const supaClient = createClient(supaUrl, supaAnonKey)
+console.log(supaClient)
