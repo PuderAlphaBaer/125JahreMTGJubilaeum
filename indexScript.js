071cd659e550ac3a0df2fedab874129db6d79986
Puderbaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-10-28 13:56:21 +0200

    grupdate

--- indexScript.js
+++ indexScript.js
@@ -12,18 +12,39 @@ const bta = document.getElementById('bta')
 const btb = document.getElementById('btb')
 const btc = document.getElementById('btc')
 const btd = document.getElementById('btd')
-var nickname;
+let nickname;
 
 
-bt1.addEventListener('click', nicknameuebermitteln());
+bt1.addEventListener('click', function () {
+    nicknameuebermitteln();
+    toggle(inputBox);
+}
+);
+
 bta.addEventListener('click', function () {console.log('a')})
 btb.addEventListener('click', function () {console.log('b')})
 btc.addEventListener('click', function () {console.log('c')})
 btd.addEventListener('click', function () {console.log('d')})
+tb1.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        bt1.click;
+    }
+});
+
+
 
 function nicknameuebermitteln() {
+    console.log('testss');
     nickname = tb1.value;
+    toggle(inputBox);
+    console.log(nickname+"test");
 }
+
+
+
+
+
 
 function toggle(obj) {
     if (obj.style.display == 'none') {
@@ -51,9 +72,13 @@ function Frage(loesung, beantwortet, start, streak) {
 };
 
 let started = false;
-let timestart = "12:00:00";
 let winstreak = 0;
+let timestart;
+
 
 let frage1 = new Frage('a', started, timestart, winstreak);
 let frage2 = new Frage('c', started, timestart, winstreak); 
 let frage3 = new Frage('c', started, timestart, winstreak);
+
+
+
