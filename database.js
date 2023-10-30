696e3ad209d08034fe367a77991c4893669723a4
Puderbaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-10-30 15:19:58 +0100

    supa

--- database.js
+++ database.js
@@ -3,49 +3,6 @@ const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 
 const database = supabase.createClient(supaUrl, supaAnonKey)
 
-// const fetchDataFromspieler = async () => {
-//     try {
-//         const { data, error } = await database
-//             .from('spieler')
-//             .select('*')
-//         if (error) {
-//             throw error
-//         }
-//         if (data) {
-//             console. log(data)
-//         }
-//     }
-//     catch (error) {
-//         console. log (error)
-//     }
-// }
-
-// fetchDataFromspieler()
-
-// const insertDataIntospieler = async (spitzname, punkte) => {
-//     try {
-//         const { res, error} = await database
-//         .from("spieler")
-//         .insert(
-//             { spieler: spitzname, punktzahl: punkte},
-//         )
-//         .select()
-//         if (error) {
-//             throw error
-//         }
-//         if (res) {
-//             console.log(res)
-//         }
-//     }
-//     catch (error) {
-//         console.log(error)
-//         if (error.code == '23505' ) {
-//             alert('Spitzname schon vergeben, bitte suchen einen anderen aus')
-//         }
-//     }
-// }
-
-
 
 const supabaseFetch = async (table, [columns]) => {
     try {
@@ -66,11 +23,42 @@ const supabaseFetch = async (table, [columns]) => {
 
 supabaseFetch('spieler', ['spieler'])
 
-// const supabaseInsert = async (table, [columns], [values]) => {
-//     try {
-//         const {data, error} = await database
-//             .from(table)
-//             .insert(
-//                 if (i)
-//             )
-//     }
+const supabaseInsert = async (table, columns, values) => {
+    try {
+        const {data, error} = await database
+            .from(table)
+            .insert(
+                rowFormatter(columns, values)
+            )
+            .select()
+        if (data) {
+            console.log('success inserting', data)
+        }
+        if (error) {
+            throw error
+        }
+    }
+    catch (error) {
+        console.log(error)
+        errorHandling(error, table)
+    }
+}
+
+function errorHandling (error, table) {
+    if (table == 'spieler') {
+        if (error.code == '23505') {
+            alert('Spitzname schon vergeben, bitte suchen einen anderen aus')
+        }
+    }
+}
+    
+
+
+function rowFormatter (columns, values) {
+    let row = {}
+    for (let i = 0; i < columns.length; i++) {
+        row[columns[i]] = values[i]
+    }
+    return row
+}
+
