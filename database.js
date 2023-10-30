82e568ded5e560cf9bc5a7b6004b8b9ed91588c8
Puderbaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-10-30 23:54:36 +0100

    bitte nutzer website nicht in ordner verschieben

--- database.js
+++ database.js
@@ -2,16 +2,33 @@ const supaUrl = 'https://cqlueytrxqlhdvhqqyse.supabase.co'
 const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbHVleXRyeHFsaGR2aHFxeXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNTQ5NTAsImV4cCI6MjAxMzgzMDk1MH0.Gr19SNLr2pNQFwxkHvKhK09DN-DjglQFPzbNY_p9A9o'
 const database = supabase.createClient(supaUrl, supaAnonKey)
 
-const supabaseFetch = async (table, columns) => {
+const supabaseFetch = async (table, columns, conditionColumn, conditionValue, orderColumn, AscTrue) => {
     try {
+        if (conditionColumn == undefined) {
+            conditionColumn = ''
+            conditionValue = ''
+        }
+        if (conditionValue == undefined) {
+            conditionValue = ''
+            conditionColumn = ''
+        }
+        if (columns == undefined) {
+            columns = '*'
+        }
+        if (orderColumn == undefined) {
+            orderColumn = 'id'
+        }
+
         const { data, error } = await database
             .from(table)
             .select(columns)
+            .eq(conditionColumn, conditionValue)
+            .order(orderColumn, {ascending: AscTrue})
         if (data) {
             console.log('success fetching', data)
         }
         if (error) {
-            throw error
+        throw error
         }
     }
     catch (error) {
@@ -39,6 +56,31 @@ const supabaseInsert = async (table, columns, values) => {
         errorHandling(error, table)
     }
 }
+
+const supabaseUpdate = async (table, columns, values, conditionColumn, conditionValue) => {
+    try {
+        const {data, error} = await database
+            .from(table)
+            .update(
+                rowFormatter(columns, values)
+            )
+            .eq(conditionColumn, conditionValue)
+            .select()
+        if (data) {
+            console.log('success updating', data)
+        }
+        if (error) {
+            throw error
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
 
 function errorHandling (error, table) {
     if (table == 'spieler') {
