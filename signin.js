39340fb38835b7c29487060c13a0e4d65de8a0e8
Frank Walter <123561496+INFOMEBIS@users.noreply.github.com>
2023-11-04 14:49:31 +0100

    keine leerzeichen mehr im UN erlaubt, bugs behoben

--- signin.js
+++ signin.js
@@ -1,6 +1,7 @@
 const submit = document.getElementById('submit');
 let un;
-let unerror = document.getElementById('unerror')
+let unerror = document.getElementById('unerror');
+blocksubmit = true;
 
 submit.addEventListener('click', function () {
     nicknameuebermitteln();
@@ -15,28 +16,39 @@ tb1.addEventListener("keypress", function(event) {
 });
 
 
-function nicknameuebermitteln() {;
+function nicknameuebermitteln() {
+    if (blocksubmit==true) {
+        tb1.classList.add('error');
+    } else {
     window.nickname = tb1.value;
     toggle(inputBox);
     supabaseInsert("spieler", ["name", "punktzahl"], [nickname, 0]) 
-}
-
+}}
 
 let data;
 function checkusername() {
-
+    unerror.style.color = "gray";
+    blocksubmit = true;
+    tb1.classList.remove('error');
     supabaseFetch("spieler", "id", "name", tb1.value, "id", true);
     setTimeout(() => {
         if(length==0) {
             unerror.style.color = "green";
             unerror.innerHTML = "Benutzername verfügbar";
+            blocksubmit = false;
         } else {
             unerror.style.color = "red";
             unerror.innerHTML = "Benutzername beireits vergeben";
+            blocksubmit = true;
         }
+        if (/\s/.test(tb1.value)) {
+            unerror.style.color = "red";
+            unerror.innerHTML = "Benutzername darf keine Leerzeichen enthalten"
+            blocksubmit = true;}
         if (!tb1.value.replace(/\s/g, '').length) {
             unerror.style.color = "red";
             unerror.innerHTML = "Benutzername muss mindestens 1 Zeichen lang sein"
+            blocksubmit = true;
         }
     }, 100);
 
@@ -44,18 +56,6 @@ function checkusername() {
 
 }
 ;
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
