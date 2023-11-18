626af6fa09aac19c8feeb5f2fb12ce9c3b3e9f2d
Puderbaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-11-18 21:27:06 +0100

    hehehe

--- questions.js
+++ questions.js
@@ -1,13 +1,14 @@
 
 // Konstruktor für Multiple Choice Fragen
 class Frage {
-    constructor(frage, a, b, c, d, loesung) {
+    constructor(frage, a, b, c, d, loesung, punkte) {
       this.frage = frage;
       this.a = a;
       this.b = b;
       this.c = c;
       this.d = d;
       this.loesung = loesung;
+      this.punkte = punkte;
     }
   }
 
@@ -15,16 +16,16 @@ class Frage {
     // Hier alle Fragen in richtiger Reinfolge auflisten
 
     // Normale Frage 
-    new Frage("Sonne", "rot", "gelb", "grün", "blau", "b"),
+    new Frage("Sonne", "rot", "gelb", "grün", "blau", "b", ''),
 
     // Frage mit zwei Lösungen
-    new Frage("Liegestütze", "Herr Krois", "Herr Pleger", "Frau Ager", "Herr Markl", ["d", "a"]),
+    new Frage("Liegestütze", "Herr Krois", "Herr Pleger", "Frau Ager", "Herr Markl", ["d", "a"], ''),
 
     // Frage mit nur 3 Antwortmöglichkeiten
-    new Frage("Tilman", "gut", "besser", "am besten", "", "c"),
+    new Frage("Tilman", "gut", "besser", "am besten", "", "c", ''),
 
     // Ja Nein Frage
-    new Frage("Christian ist ein Profi", "Ja", "Nein", "", "", "a"),
+    new Frage("Christian ist ein Profi", "Ja", "Nein", "", "", "a", ''),
   ];
 
 
--- test/snippets.js
+++ test/snippets.js
@@ -2,19 +2,43 @@ const frage1 = document.getElementById('frage1');
 const frage2 = document.getElementById('frage2');
 const frage3 = document.getElementById('frage3');
 
-//async function that checks if a question already started
-// const checkIfQuestionStarted = async (questionId) => {
-//     await supabaseFetch('fragen', 'start', 'eq', 'id', questionId)
-//     console.log('data', data)
-// }
-
-frage1.addEventListener('click', function() {
-  alert('Frage 1');
-})
+const checkIfQuestionStarted = async (questionId) => {
+    const data = await supabaseFetch('fragen', 'start', 'eq', 'id', questionId);
+    start = await data[0].start;
+    if (start>0) {
+        console.log('Frage '+questionId+' gestartet um '+milliUTCToLocal(start));
+        return true;
+    }
+}
+async function ligma() {
+    const start2 = await checkIfQuestionStarted(2);
+    const start1 = await checkIfQuestionStarted(1);
+    const start3 = await checkIfQuestionStarted(3);
+    if (start1==true) {
+        frage1.innerHTML = 'Frage 1 gestartet';
+    }
+    if (start2==true) {
+        frage2.innerHTML = 'Frage 2 gestartet';
+    }
+    if (start3==true) {
+        frage3.innerHTML = 'Frage 3 gestartet';
+    }
+}
+ligma();
 
 function milliUTC() {
     return jetzt().getTime() + (jetzt().getTimezoneOffset() * 60000);
 }
+
+frage1.addEventListener('click', function() {
+    if (checkIfQuestionStarted(1)==true) {
+        return;
+    }
+    supabaseUpdate('fragen', ['start'], [milliUTC()], 'eq', 'id', 1);
+    alert('Frage 1 gestartet');
+})
+
+
 
 function jetzt() {
     return new Date()
