6e2913bc397e37300afe718b6cd480457e400ff5
Puderbaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-11-29 10:36:59 +0100

    ja

--- value.js
+++ value.js
@@ -22,21 +22,32 @@ const pretime = 6000;
     // Hier alle Fragen in richtiger Reinfolge auflisten
 
     // Unwichtig, kann weg glaub ich, mach du lieber sonst bin ich schuld
-    new Frage("Spiel", "Spiel", "Spiel", "Spiel", "Spiel", "a", 5,  false, false, false),
+    new Frage("", "", "", "", "", "", 0,  false, false, false),
 
     new Frage("Wer war der erste Mensch im Weltraum?", "Alexander Gerst", "Yuri Gagarin", "Neil Armstrong", "Herr Eschrich", "b", 20, false, false, false),
 
-    new Frage("Wie viele Lösungen in Kahoot Quiz mit Frau Weber?", "1", "2", "3", "alle", ["a", "b", "c", "d"], 10, false, false, false),
+    // new Frage("Wie viele Lösungen in Kahoot Quiz mit Frau Weber?", "1", "2", "3", "alle", ["a", "b", "c", "d"], 10, false, false, false),
 
-    //new Frage("Carlotta ist die BESTE Testerin", "ist ja normal", "RICHTIG", "natuerlich", "ne", ['a', 'b', 'c'], 10, 5, false, false, false),
+    new Frage("Carlotta ist die BESTE Testerin", "ist ja normal", "RICHTIG", "natuerlich", "ne", ['a', 'b', 'c'], 10, 5, false, false, false),
 
-    //new Frage ("Luzia ist auch eine sehr tolle Testerin", "Ja", "Nein", "", "", "a", 10, 6, false, false, false),
+    new Frage ("Luzia ist auch eine sehr tolle Testerin", "Ja", "Nein", "", "", "a", 10, 6, false, false, false),
 
-    new Frage ('Was heisst Sklave auf Latein?', 'Sklave', 'Sklavus', 'Servus', 'Slave', 'c', 10, false, false, false),
+    new Frage ('Wann begann die Französische Revolution?', '1789', '1788', '1787', '1786', 'a', 10, 8, false, false, false),
 
-    new Frage("Sonne", "grün", "rot", "blau", "gelb", "d", 5, 1, false, false, false),
+    new Frage ('Was heisst Sklave auf Latein?', 'Sklave', 'Sklavus', 'Servus', '', 'c', 10, false, false, false),
 
-    new Frage ('Wie viele Bytes sind in einem Kilobyte?', '1000', '1024', '1032', '1048', 'b', 20, false, false, false),
+    // new Frage("Sonne", "grün", "rot", "blau", "gelb", "d", 5, 1, false, false, false),
+    new Frage ('Wann wurde die Bundesrepublik Deutschland gegründet?', '1945', '1946', '1947', '1948', 'd', 10, 12, false, false, false),
+
+    new Frage ('Wann wurde die DDR gegründet?', '1945', '1946', '1947', '1948', 'b', 10, 13, false, false, false),
+
+    new Frage ('Wann begann der 2. Weltkrieg?', '1938', '1939', '1940', '1941', 'b', 10, 15, false, false, false),
+
+    new Frage ('Wer war der erste Bundeskanzler der BRD?', 'Konrad Adenauer', 'Helmut Kohl', 'Angela Merkel', 'Otto von Bismarck', 'a', 10, 16, false, false, false),
+
+    new Frage ('Wer war der erste Reichskanzler des Deutschen Reiches?', 'Otfried Biscuits', 'Alice Weidel', '', 'Otto von Bismarck', 'd', 10, 17, false, false, false),
+
+    new Frage ('Wie viele Bytes sind in einem Kibibyte?', '1000', '1024', '1032', '1048', 'b', 20, false, false, false),
 
     new Frage ('Was ist die Wurzel aus 196?', '14', '13', '15', '35/2', 'a', 20, false, false, false),
 
@@ -46,7 +57,8 @@ const pretime = 6000;
 
     new Frage ("Wohin gehe ich wenn ich Streit habe?", "Sanitäter", "Frau Bertram", "Umweltscouts", "Mediatoren", "d", 20, false, false, false),
 
-    new Frage ("a² + b² = c² ist...", "die Mitternachtsformel", "eine binomische Formel", "der Satz des Phytagoras", "der Satz des Thales", "c", 20, false, false, false)
+    // new Frage ("a² + b² = c² ist...", "die Mitternachtsformel", "eine binomische Formel", "der Satz des Phytagoras", "der Satz des Thales", "c", 20, false, false, false)
+
 
   ];
 
