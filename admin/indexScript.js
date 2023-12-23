e5bc6e8b4e1926cc4c90c44b36f47d5ee84c3a15
Chriveloper <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-12-23 14:12:44 +0100

    aha

---
+++ admin/indexScript.js
@@ -0,0 +1,71 @@
+const rangliste = document.getElementById('rangliste');
+const ubox = document.getElementById('ubox');
+let repbox = "";
+
+
+
+let uclass = "udata"
+// Hier einfach userupdate() mit supabase verbinden
+
+
+function userupdate(uid, rank, uname, score, blocked) {
+    
+if(blocked != null) {
+    uclass = "banned"; 
+    repbox = "";
+} 
+else {
+    uclass = "udata"
+    repbox = `<td class="ureport" onclick="report(${uid}, '${uname}')">REPORT</td>`
+}
+
+
+    ubox.innerHTML += `
+    <tr>
+        <td class="${uclass}">${uid}</th>
+        <td class="${uclass}">${uname}</th>
+        <td class="${uclass}">${score}</th>
+        <td class="${uclass}">${rank}</th>
+        ${repbox}
+    </tr>`;
+}
+
+
+let ban;
+
+ function report(uid, uname) {
+    // if (confirm(`Sind Sie sich sicher, dass Sie User ${uid} mit dem Namen "${uname}" sperren möchten`)) {
+    //     supabaseUpdate("spieler", ["blocked", "punkte"], [true, -1], "eq",  "id",  uid).then(() => {
+    //         fetchUserlist();
+    //     });
+    // }
+    
+    
+
+    ban = prompt(`Wofür wollen Sie User ${uid} mit dem Namen "${uname}" sperren`,  "Anstößiger Benutzername");
+    if ((ban )== null || ban == "") {
+      console.log("bann abgebrochen");
+    } else {
+        supabaseUpdate("spieler", ["blocked", "punkte", "streak"], [ban, -1, 0], "eq",  "id",  uid).then(() => {
+            fetchUserlist();
+        });
+    }
+
+    };
+
+function fetchUserlist() {
+    ubox.innerHTML =`<tr> 
+    <th class="udata">UID</th>
+    <th class="udata">User Name</th>
+    <th class="udata">Score</th>
+    <th class="udata">Rank</th>
+  </tr>`;
+    supabaseFetch('spieler', 'id, name, punkte, rang, blocked', '', '', "", 'rang', false).then((data) => {
+                console.log(data)
+         for (let i = 0; i < data.length; i++) {
+            userupdate(data[i].id, i + 1, data[i].name, data[i].punkte, data[i].blocked)
+         }
+        });
+    }
+
+    fetchUserlist();
---
+++ screen/user.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(id, name, punkte, streak, rank, blocked, afk, podium) {
+        this.id = id;
+        this.name = name;
+        this.punkte = punkte;
+        this.streak = streak;
+        this.rank = rank;
+        this.blocked = blocked;
+        this.afk = afk;
+        this.podium = podium;
+    }
+}
+
+
+
+userlist = [
+
+];
