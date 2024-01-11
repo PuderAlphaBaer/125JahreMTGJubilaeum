1af33182a9bf21a3a6d097583381aa573c11aae6
Marrrrrrkus <infohack222@gmail.com>
2024-01-11 14:37:28 +0100

    pw schnell geamcht

---
+++ password.js
@@ -0,0 +1,6 @@
+pw = prompt("Passwort eingeben:",  "TilmanderKÖNNER");
+if (pw == "kackPseminar") {
+    console.log("pw richtig");
+} else {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
