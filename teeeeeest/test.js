a3f54dff23222908af124d38fcfa4f23a43d76a1
Marrrrrrkus <infohack222@gmail.com>
2024-01-14 21:52:42 +0100

    eeendlich fix confetti und noch weiter animation

--- teeeeeest/test.js
+++ teeeeeest/test.js
@@ -1,10 +1,10 @@
 const fl = document.getElementById('fl');
 const dark = document.getElementById('dark');
 const name1 = document.getElementById('name1');
+const name2 = document.getElementById('name2');
 
 
-
-function rank1() {
+function podwin() {
     dark.style.opacity = 1;
     setTimeout(() => {
         fl.style.display = "flex";
@@ -91,12 +91,14 @@ function conf1() {
       Object.assign({}, defaults, {
         particleCount,
         origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        colors: ['#ef8d0a', '#56a51a', '#0955a4']
     })
     );
     confetti(
       Object.assign({}, defaults, {
         particleCount,
         origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        colors: ['#ef8d0a', '#56a51a', '#0955a4']
       })
     );
   }, 250);
@@ -107,328 +109,51 @@ function conf1() {
 
 
 
-// import { tsParticles } from "https://cdn.jsdelivr.net/npm/@tsparticles/engine@3.0.3/+esm";
-// import { loadAll } from "https://cdn.jsdelivr.net/npm/@tsparticles/all@3.0.3/+esm";
 
-// async function loadParticles(options) {
-//   await loadAll(tsParticles);
 
-//   await tsParticles.load({
-//           id: "tsparticles",
-//           options: {
-//   "fullScreen": {
-//     "zIndex": 1
-//   },
-//   "emitters": {
-//     "position": {
-//       "x": 50,
-//       "y": 100
-//     },
-//     "rate": {
-//       "quantity": 5,
-//       "delay": 0.15
-//     }
-//   },
-//   "particles": {
-//     "color": {
-//       "value": [
-//         "#1E00FF",
-//         "#FF0061",
-//         "#E1FF00",
-//         "#00FF9E"
-//       ]
-//     },
-//     "move": {
-//       "decay": 0.05,
-//       "direction": "top",
-//       "enable": true,
-//       "gravity": {
-//         "enable": true
-//       },
-//       "outModes": {
-//         "top": "none",
-//         "default": "destroy"
-//       },
-//       "speed": {
-//         "min": 50,
-//         "max": 100
-//       }
-//     },
-//     "number": {
-//       "value": 1500
-//     },
-//     "opacity": {
-//       "value": 1
-//     },
-//     "rotate": {
-//       "value": {
-//         "min": 0,
-//         "max": 360
-//       },
-//       "direction": "random",
-//       "animation": {
-//         "enable": true,
-//         "speed": 30
-//       }
-//     },
-//     "tilt": {
-//       "direction": "random",
-//       "enable": true,
-//       "value": {
-//         "min": 0,
-//         "max": 360
-//       },
-//       "animation": {
-//         "enable": true,
-//         "speed": 30
-//       }
-//     },
-//     "size": {
-//       "value": 3,
-//       "animation": {
-//         "enable": true,
-//         "startValue": "min",
-//         "count": 1,
-//         "speed": 16,
-//         "sync": true
-//       }
-//     },
-//     "roll": {
-//       "darken": {
-//         "enable": false,
-//         "value": 25
-//       },
-//       "enlighten": {
-//         "enable": false,
-//         "value": 25
-//       },
-//       "enable": true,
-//       "speed": {
-//         "min": 5,
-//         "max": 15
-//       }
-//     },
-//     "wobble": {
-//       "distance": 30,
-//       "enable": true,
-//       "speed": {
-//         "min": -7,
-//         "max": 7
-//       }
-//     },
-//     "shape": {
-//       "type": [
-//         "circle",
-//         "square"
-//       ],
-//       "options": {}
-//     }
-//   },
-//   "responsive": [
-//     {
-//       "maxWidth": 1024,
-//       "options": {
-//         "particles": {
-//           "move": {
-//             "speed": {
-//               "min": 33,
-//               "max": 66
-//             }
-//           }
-//         }
-//       }
-//     }
-//   ]
-// }
-//       });
-// }
 
-// const emitterRate = {
-//     delay: 0.1,
-//     quantity: 200
-//   },
-//   options = {
-//     particles: {
-//       opacity: {
-//         value: 1
-//       },
-//       size: {
-//         value: {
-//           min: 16,
-//           max: 32
-//         }
-//       },
-//       move: {
-//         enable: true,
-//         gravity: {
-//           enable: true
-//         },
-//         speed: 15,
-//         outModes: {
-//           default: "destroy",
-//           top: "none"
-//         }
-//       },
-//       number: {
-//         value: 500
-//       },
-//       rotate: {
-//         value: {
-//           min: 0,
-//           max: 360
-//         },
-//         direction: "random",
-//         move: true,
-//         animation: {
-//           enable: true,
-//           speed: 60
-//         }
-//       },
-//       tilt: {
-//         direction: "random",
-//         enable: true,
-//         move: true,
-//         value: {
-//           min: 0,
-//           max: 360
-//         },
-//         animation: {
-//           enable: true,
-//           speed: 60
-//         }
-//       },
-//       roll: {
-//         darken: {
-//           enable: true,
-//           value: 30
-//         },
-//         enlighten: {
-//           enable: true,
-//           value: 30
-//         },
-//         enable: true,
-//         mode: "both",
-//         speed: {
-//           min: 15,
-//           max: 25
-//         }
-//       },
-//       wobble: {
-//         distance: 30,
-//         enable: true,
-//         move: true,
-//         speed: {
-//           min: -15,
-//           max: 15
-//         }
-//       }
-//     },
-//     background: {
-//       color: "#000000"
-//     },
-//     emitters: [
-//       {
-//         position: {
-//           x: 0,
-//           y: 33
-//         },
-//         rate: emitterRate,
-//         particles: {
-//           move: {
-//             direction: "top-right"
-//           },
-//           shape: {
-//             type: "emoji",
-//             options: {
-//               emoji: {
-//                 value: "🦄"
-//               }
-//             }
-//           }
-//         }
-//       },
-//       {
-//         position: {
-//           x: 0,
-//           y: 66
-//         },
-//         rate: emitterRate,
-//         particles: {
-//           move: {
-//             direction: "top-right"
-//           },
-//           shape: {
-//             type: "emoji",
-//             options: {
-//               emoji: {
-//                 value: "🌈"
-//               }
-//             }
-//           }
-//         }
-//       },
-//       {
-//         position: {
-//           x: 100,
-//           y: 33
-//         },
-//         rate: emitterRate,
-//         particles: {
-//           move: {
-//             direction: "top-left"
-//           },
-//           shape: {
-//             type: "emoji",
-//             options: {
-//               emoji: {
-//                 value: "🎉"
-//               }
-//             }
-//           }
-//         }
-//       },
-//       {
-//         position: {
-//           x: 100,
-//           y: 66
-//         },
-//         rate: emitterRate,
-//         particles: {
-//           move: {
-//             direction: "top-left"
-//           },
-//           shape: {
-//             type: "emoji",
-//             options: {
-//               emoji: {
-//                 value: "🤡"
-//               }
-//             }
-//           }
-//         }
-//       },
-//       {
-//         position: {
-//           x: 50,
-//           y: 50
-//         },
-//         rate: emitterRate,
-//         particles: {
-//           move: {
-//             direction: "top"
-//           },
-//           shape: {
-//             type: "emoji",
-//             options: {
-//               emoji: {
-//                 value: "👻"
-//               }
-//             }
-//           }
-//         }
-//       }
-//     ]
-//   };
+function rank1(){
+  var theLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZÖÄÜ1234567890"; //You can customize what letters it will cycle through
+  var ctnt = "Tilmanofatz"; // Your text goes here
+  var speed = 50; // ms per frame
+  var increment = 8; // frames per step. Must be >2
+  
+      
+  var clen = ctnt.length;       
+  var si = 0;
+  var stri = 0;
+  var block = "";
+  var fixed = "";
+  //Call self x times, whole function wrapped in setTimeout
+  
+  (function rustle (i) {          
+  setTimeout(function () {
+    if (--i){rustle(i);}
+    nextFrame(i);
+    si = si + 1;        
+  }, speed);
+  })(clen*increment+1); 
+  function nextFrame(pos){
+    for (var i=0; i<clen-stri; i++) {
+      //Random number
+      var num = Math.floor(theLetters.length * Math.random());
+      //Get random letter
+      var letter = theLetters.charAt(num);
+      block = block + letter;
+    }
+    if (si == (increment-1)){
+      stri++;
+    }
+    if (si == increment){
+    // Add a letter; 
+    // every speed*10 ms
+    fixed = fixed +  ctnt.charAt(stri - 1);
+    si = 0;
+    }
+    name2.innerHTML = (fixed + block);
+    block = "";
+  }
+  };
 
-// loadParticles(options);
 
+  console.log("funktionen rank1() und podwin() aufrufen")
